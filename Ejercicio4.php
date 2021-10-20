4832e2a337f1fc932e27622e2b6325d0e7cb592b
kikoirles <cristianirles14@gmail.com>
2021-10-20 20:37:38 +0200

    IMC

---
+++ Ejercicio4.php
@@ -0,0 +1,8 @@
+<?php
+
+/*4. Crea una función que reciba tres parámetros (nombre de producto, precio e impuesto en porcentaje sobre 100). La función hará lo siguiente:
+
+   * Los parámetros deberán tener un valor por defecto por si no los recibe que deben ser: "Producto genérico", 100 y 21.
+   * Si los números no son válidos (NaN) muestra un error. Si son válidos, muestra por consola el nombre del producto y el precio final contando impuestos.*/
+
+create function
