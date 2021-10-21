aef4a55a57b509d92ea9606bc8523f91e5e6856f
kikoirles <cristianirles14@gmail.com>
2021-10-21 17:40:45 +0200

    IMC

--- Ejercicio4.php
+++ Ejercicio4.php
@@ -5,4 +5,31 @@
    * Los parámetros deberán tener un valor por defecto por si no los recibe que deben ser: "Producto genérico", 100 y 21.
    * Si los números no son válidos (NaN) muestra un error. Si son válidos, muestra por consola el nombre del producto y el precio final contando impuestos.*/
 
-create function
+$arrayproductos=0;
+$preciofinal=0;
+$producto=0; 
+$precio=0; 
+$impuesto=0;
+
+function producto_valido ($producto, $precio, $impuesto){
+	if ($producto = null){
+        echo "NaN";}
+    elseif ($precio = null){
+    	echo "NaN";}
+    elseif ($impuesto > 100 and $impuesto < 0){
+    	echo "NaN";}
+    else $preciofinal=($precio+($precio*$impuesto/100));
+
+    return $preciofinal;}
+
+$arrayproductos;
+
+ foreach ($_POST as $clave => $valor){
+    echo "$clave es: $valor <br>";}
+    $arrayproductos = producto_valido ($producto,$precio,$impuesto);
+  
+
+
+echo "<br>";
+echo "Precio final ";
+print_r ($arrayproductos);
