b76d6b19bc264c08f6396e540231ae8bdb229e42
kikoirles <cristianirles14@gmail.com>
2021-10-26 17:00:04 +0200

    IMC

--- MYSQL/ejemploConecxion.php
+++ MYSQL/ejemploConecxion.php
@@ -1,31 +1,40 @@
 <?php
 
-//Proceso de conexion a la base de datos 
+//Proceso de conexión a la base de datos
 $conexion = mysqli_connect("localhost","christian","barcelona22","bd_ejemplo");
 
-if ($conexion = false){
-	echo "<br> La conexion se ha realizado de forma correcta</br>";	}
-else{
-	echo "<br> Ha habido un problema al conectar a la base de datos.";}
+if ($conexion!=false){
+    echo "<br>La conexión se ha realizado de forma correcta</br>";
+}else{
+    echo "<br> Ha habido un problema al conectar a la base de datos.";
+}
 
-//Ejemplo de lectura de datos a MYSQL desde PHP
+//Ejemplo de inserción de datos a MySQL desde PHP
+$query="INSERT INTO entrenador VALUES (8,'Michel Jordan',54)";
+$resultado = mysqli_query($conexion,$query);
+
+
+
+//Ejemplo de lectura de una base de datos
 $query = "SELECT * FROM entrenador";
 $resultado = mysqli_query($conexion,$query);
 
-while ($fila =mysql_fetch_array($resultado,MYSQLI_BOTH)){
-	print_r($fila);
-	echo "<br>";}
+while ($fila = mysqli_fetch_array($resultado,MYSQLI_ASSOC)){
+    print_r($fila);
+    echo "<br>";
+}
 
-//Ejemplo de insercion de datos a MYSQL desde PHP
-$query="INSERT INTO entrenador VALUES (8, 'Michael Jordan' ,54)";
-$resultado = mysqli_query($conexion,$query);
+
 
 if ($resultado){
-	echo "<br> La insercion se ha realizado de forma correcta</br>";}
-else{
-	echo "<br> Ha habido un problema alejecutar la consulta";}
+    echo "La inserción se ha realizado de forma correcta";
+}else{
+    echo "La inserción de datos no se ha podido realizar";
+}
+    
 
-//proceso de desconexion de la base de datos 
-if($conexion != false){
-	mysql_close($conexion);}
 
+//Proceso de desconexión de la base de datos
+if($conexion!=false){
+    mysqli_close($conexion);
+}
